23638276b0ede1bf1cecbbcc234abf55fe8cd0d2
Almos11 <amosin2002@mail.ru>
2023-02-20 11:08:09 +0300

    Add time tracking

--- securing-web/src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ securing-web/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -1,11 +1,18 @@
-
 package com.example.securingweb;
 
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +20,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -33,7 +44,8 @@ public class WebSecurityConfig {
                         .failureUrl("/login?error")
                         .permitAll()
                 )
-                .logout((logout) -> logout.permitAll());
+                .logout((logout) -> logout.permitAll())
+                .addFilterBefore(new AuthenticationFilter(), AbstractPreAuthenticatedProcessingFilter.class);
 
         return http.build();
     }
@@ -42,6 +54,7 @@ public class WebSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user1 =
@@ -56,5 +69,29 @@ public class WebSecurityConfig {
                         .build();
 
         return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    private class AuthenticationFilter implements Filter {
+
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+            HttpServletRequest request = (HttpServletRequest) servletRequest;
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication != null && authentication.isAuthenticated()) {
+                WebAuthenticationDetails details = (WebAuthenticationDetails) authentication.getDetails();
+                long authenticationTime = details.getRemoteAddress().hashCode(); // Получение уникального значения, например, хэша IP-адреса
+                long currentTime = System.currentTimeMillis();
+                long diff = (currentTime - authenticationTime) / 1000;
+                if (diff <= 15) {
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                }
+
+            } else {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            }
+        }
     }
 }
