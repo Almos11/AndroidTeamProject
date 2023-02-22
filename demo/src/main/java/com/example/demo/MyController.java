7588227611ea0f175ae6b9a17d64df79fa0e8bb9
Almos11 <amosin2002@mail.ru>
2023-02-23 01:22:30 +0300

    Add token

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -2,29 +2,47 @@ package com.example.demo;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 @RestController
 public class MyController {
-    public static Map<String, String> list_users;
+    public static Map<String, String> listUsers;
     static {
-        list_users = new HashMap<>();
-        list_users.put("user1", "password1");
-        list_users.put("user2", "password2");
+        listUsers = new HashMap<>();
+        listUsers.put("user1", "password1");
+        listUsers.put("user2", "password2");
+    }
+    static Map<String, User> listUsersWhoHaveToken;
+    static {
+        listUsersWhoHaveToken = new HashMap<>();
     }
 
     @GetMapping("/login")
     public ResponseEntity<String> login(@RequestParam String username, @RequestParam String password) {
-        if (!list_users.containsKey(username)) {
+        if (!listUsers.containsKey(username)) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
-        if (!(list_users.get(username).equals(password))) {
+        if (!(listUsers.get(username).equals(password))) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
-        String randomString = UUID.randomUUID().toString();
-        return ResponseEntity.ok(randomString);
+        String token;
+        if (listUsersWhoHaveToken.containsKey(username)) {
+            token = listUsersWhoHaveToken.get(username).getToken();
+        } else {
+            token = UUID.randomUUID().toString();
+            User user = new User(username, token);
+            listUsersWhoHaveToken.put(username, user);
+            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(user,
+                    null, new ArrayList<>()));
+        }
+        return ResponseEntity.ok(token);
     }
+
 }
---
+++ demo/src/main/java/com/example/demo/User.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+public class User {
+    private String username;
+    private String token;
+
+    public User(String username, String token) {
+        this.username = username;
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
+
