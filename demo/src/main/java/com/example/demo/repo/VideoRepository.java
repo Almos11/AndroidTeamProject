89eb582377702188d29b378b975f61caadaa5ed9
Almos11 <amosin2002@mail.ru>
2023-04-05 11:53:24 +0300

    Syntactic edit

--- demo/src/main/java/com/example/demo/repo/VideoRepository.java
+++ demo/src/main/java/com/example/demo/repo/VideoRepository.java
@@ -1,6 +1,5 @@
 package com.example.demo.repo;
 
-import com.example.demo.models.UserDataBase;
 import com.example.demo.models.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
