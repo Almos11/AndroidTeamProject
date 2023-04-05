34c3bbfd5f6ceab62110871d5a6cf5f3cd20324f
Almos11 <amosin2002@mail.ru>
2023-04-05 11:09:19 +0300

    Syntactic edit

--- demo/src/main/java/com/example/demo/repo/VideoRepository.java
+++ demo/src/main/java/com/example/demo/repo/VideoRepository.java
@@ -3,31 +3,20 @@ package com.example.demo.repo;
 import com.example.demo.models.UserDataBase;
 import com.example.demo.models.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
-    @Transactional
-    public Video findVideoByName(String nameVideo);
 
     @Transactional
-    public Video findVideoById(Long id);
-
-
-    @Transactional
-    public Video findVideoByUserDataBaseAndId(UserDataBase user, Long id);
+    Video findVideoByIdentifier(String identifier);
 
     @Transactional
-    public Video findVideoByIdentifier(String identifier);
-
-    @Transactional
-    public List<Video> findAll();
+    List<Video> findAll();
 
     @Transactional
     @Query(value = "SELECT identifier FROM videos ORDER BY LOG(rating) DESC LIMIT 1", nativeQuery = true)
