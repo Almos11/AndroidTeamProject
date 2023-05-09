b1766f51aabba52e93c3d2e4e74f3acd7b67bb79
Almos11 <amosin2002@mail.ru>
2023-05-09 11:51:29 +0300

    Run video - (no work)

--- Client/app/src/main/java/com/example/client/MainActivity.java
+++ Client/app/src/main/java/com/example/client/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
     public void nextScreen(View v) {
         Intent intent = new Intent(this, SecondScreen.class);
         startActivity(intent);
+    }
+
+    public void runVideo(View v) {
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        String videoUrl = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+        try {
+            mediaPlayer.setDataSource(videoUrl);
+            mediaPlayer.setOnPreparedListener(MediaPlayer::start);
+            mediaPlayer.prepareAsync();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mediaPlayer.release();
+        mediaPlayer = null;
     }
 }
