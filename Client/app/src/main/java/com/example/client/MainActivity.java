03030d5d7d14a46e5c9e700cbaa8cdacbbf2d700
Almos11 <amosin2002@mail.ru>
2023-05-12 14:52:14 +0300

    video playback - done

--- Client/app/src/main/java/com/example/client/MainActivity.java
+++ Client/app/src/main/java/com/example/client/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
 
 import java.io.IOException;
 
@@ -25,17 +28,20 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void runVideo(View v) {
-        MediaPlayer mediaPlayer = new MediaPlayer();
-        String videoUrl = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
-        try {
-            mediaPlayer.setDataSource(videoUrl);
-            mediaPlayer.setOnPreparedListener(MediaPlayer::start);
-            mediaPlayer.prepareAsync();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        mediaPlayer.release();
-        mediaPlayer = null;
+    public void runVideo(View view) {
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video1;
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath(videoPath);
+        videoView.start();
     }
+
+    public void runVideoURL(View view) {
+        String videoUrl = "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoURI(Uri.parse(videoUrl));
+        videoView.start();
+    }
+
+
+
 }
